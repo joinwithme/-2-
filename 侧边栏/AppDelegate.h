a2138c4a0789e8ea7c7f8baf2df33efc00e0e8aa
qianfeng <qianfeng@hh.local>
2015-08-01 14:30:07 -0400

    Initial Commit

---
+++ 侧边栏/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  侧边栏
+//
+//  Created by qianfeng on 15/8/1.
+//  Copyright (c) 2015年 郭振伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 侧边栏/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  侧边栏
+//
+//  Created by qianfeng on 15/8/1.
+//  Copyright (c) 2015年 郭振伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
